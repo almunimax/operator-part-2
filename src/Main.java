689ee5fb308cb8453e45647ad86b7e589c76ce79
almunimax <almunimax@mail.ru>
2022-12-04 13:56:30 +0300

    ус.оператор часть 2

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // задание 1
+        System.out.println("задание 1");
+        int ios = 0;
+        int android = 1;
+        boolean clientOS = ios < android;
+        if (clientOS){
+            System.out.println("скачать приложение для ios по ссылке");
+        }
+        else {
+            System.out.println("скачать приложение для android по ссылке");
+        }
+
+            }
+
+        }
